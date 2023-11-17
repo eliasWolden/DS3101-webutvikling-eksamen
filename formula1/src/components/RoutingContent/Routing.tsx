6c27cd0a9b0b31b2452ea1cfaa9340ca1890c4f2
eliasWolden <eliasbwolden@live.com>
2023-11-17 16:58:47 +0100

    navbar laget :)

--- formula1/src/components/RoutingContent/Routing.tsx
+++ formula1/src/components/RoutingContent/Routing.tsx
@@ -1,67 +1,78 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { DriverPage, HomePage, RacePage, TeamPage } from "../../pages";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { DriverProvider } from "../../contexts/DriverContext";
 import { RaceProvider } from "../../contexts/RaceContext";
 import { TeamProvider } from "../../contexts/TeamContext";
 import CreateDriverPage from "../../pages/CreateDriverPage";
 import CreateRacePage from "../../pages/CreateRacePage";
+import '../../css/main.css';
+import { useState } from "react";
 
 function Routing() {
+  const [open, setOpen] = useState(false);
+  
   return (
     <>
       <BrowserRouter>
         <DriverProvider>
           <RaceProvider>
             <TeamProvider>
-              <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <div className="container-fluid">
-                  <Link to="/" className="navbar-brand">
-                    Home
-                  </Link>
+{!open &&
+<div className="open-button">
+  <button className="image-button" onClick={() => setOpen(!open)}>
+    <img src={"public/images/open.jpg"} alt="poen button"/>
+  </button>
+</div>
+}
 
-                  <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                  >
-                    <span className="navbar-toggler-icon"></span>
-                  </button>
-                  <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                      <li className="nav-item">
-                        <Link to="/DriverPage" className="nav-link">
-                          Driver
-                        </Link>
-                      </li>
-                      <li className="nav-item">
-                        <Link to="/RacePage" className="nav-link">
-                          Race
-                        </Link>
-                      </li>
-                      <li className="nav-item">
-                        <Link to="/TeamPage" className="nav-link">
-                          Team
-                        </Link>
-                      </li>
-                      <li className="nav-item">
-                        <Link to="/CreateDriverPage" className="nav-link">
-                          DriverPage
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to="/CreateRacePage" className="nav-link">
-                          CreateRacePage
-                        </Link>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </nav>
+{open && (
+<div className="large-box">
+  <div className="right-box" onClick={() => setOpen(!open)}></div>
+  <div className="left-box">
+    <button className="image-button" onClick={() => setOpen(!open)}>
+      <img src={"public/images/close.png"} alt="close button"/>
+    </button>
+
+  <div className="list-box">
+    <ul className="list-1-in-box">
+      <li className="list-text-1">
+        <Link to="/" className="navbar-brand">
+          Home
+          </Link>
+      </li>
+      <li className="list-text-1">
+          <Link to="/DriverPage" className="nav-link">
+            Driver
+          </Link>
+      </li>
+      <li className="list-text-1">
+          <Link to="/RacePage" className="nav-link">
+            Race
+          </Link>
+      </li>
+    </ul>
+
+    <ul className="list-2-in-box">
+      <li className="list-text-2">
+        <Link to="/TeamPage" className="nav-link">
+          Team
+        </Link>
+      </li>
+      <li className="list-text-2">
+        <Link to="/CreateDriverPage" className="nav-link">
+          DriverPage
+        </Link>
+      </li>
+      <li className="list-text-2">
+        <Link to="/CreateRacePage" className="nav-link">
+          CreateRacePage
+        </Link>
+      </li>
+    </ul>
+    </div>
+  </div>
+</div>
+            )}
               <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/DriverPage" element={<DriverPage />} />
