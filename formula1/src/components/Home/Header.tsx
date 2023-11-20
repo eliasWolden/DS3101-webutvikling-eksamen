a4698fd81c216567a592f297a12d09cf054a7520
bdybsjord <123090830+bdybsjord@users.noreply.github.com>
2023-11-20 13:20:44 +0100

    fjerna css header og countdown video filter

--- formula1/src/components/Home/Header.tsx
+++ formula1/src/components/Home/Header.tsx
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
-    <div className="header text-center p-3">
-      <div className="container">
-        <h1 className="animate-charcter">Formula 1 Special Event</h1>
+    <div className="container">
+      <div className="header text-center text-white text-uppercase">
+        <h1>Formula 1 Special Event</h1>
       </div>
     </div>
   );
--- formula1/src/components/Home/VideoBackground.tsx
+++ formula1/src/components/Home/VideoBackground.tsx
@@ -2,15 +2,17 @@ import "../../css/HomePage.css";
 
 const VideoBackground = () => {
   return (
-    <div className="video-background position-relative">
-      <video className="w-100" autoPlay muted loop>
-        <source src="./videos/Formula1Video.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-      <div className="docs-button position-absolute start-0 translate-middle-y ms-3">
-        <button className="btn btn-secondary">Docs</button>
+    <section>
+      <div className="video-background position-relative">
+        <div className="overlay"></div>
+        <video className="w-100" autoPlay muted loop>
+          <source src="./videos/Formula1Video.mp4" type="video/mp4" />
+        </video>
+        <div className="docs-button position-absolute start-0 translate-middle-y ms-3">
+          <button className="btn btn-secondary">Docs</button>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
