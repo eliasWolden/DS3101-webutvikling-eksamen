eab42afe3addddaf5a2c0b5abaa30f8cbe7550a7
Malin Akre <malin.akre01@gmail.com>
2023-11-26 20:43:53 +0100

    deletedriver

--- formula1/src/components/Drivers/DeleteDriver.tsx
+++ formula1/src/components/Drivers/DeleteDriver.tsx
@@ -1,7 +1,8 @@
-import { FC, useState, useContext } from "react";
+import { FC, useState, useContext, ChangeEvent } from "react";
 import { IGeneralContext } from "../../interfaces/IGeneralContext";
 import { IDriver } from "../../interfaces/Drivers/IDriver";
 import { GeneralContext } from "../../contexts/GeneralProvider";
+import DriverForm from "./DriverForm";
 
 const DeleteDriver: FC = () => {
   const [firstName, setFirstName] = useState("");
@@ -30,33 +31,27 @@ const DeleteDriver: FC = () => {
       setDeleteStatus("Error deleting driver");
     }
   };
+
+  const setHandler = (e: ChangeEvent<any>) => {
+      const { name, value } = e.target;
+  
+      switch (name) {
+        case "Firstname":
+          setFirstName(value);
+          break;
+        case "Lastname":
+          setLastName(value);
+          break;
+      }
+    }
+
   return (
     <section className="d-flex justify-content-center">
       <form className="d-flex flex-column align-items-center bg-light p-4 border shadow w-75 rounded mb-3">
         <h2>Delete driver</h2>
 
-        <div className="form-group col-md-4 mb-4">
-          <label htmlFor="exampleFormControlInput5">Firstname</label>
-          <input
-            type="text"
-            className="form-control"
-            id="exampleFormControlInput5"
-            placeholder="Firstname"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-        </div>
-        <div className="form-group col-md-4">
-          <label htmlFor="exampleFormControlInput6">Lastname</label>
-          <input
-            type="text"
-            className="form-control"
-            id="exampleFormControlInput6"
-            placeholder="Lastname"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          />
-        </div>
+         <DriverForm setHandler={setHandler} />
+
 
         <div className="row">
           <div className="form-group col-md-4 p-3">
@@ -66,7 +61,6 @@ const DeleteDriver: FC = () => {
               onClick={handleDelete}
               value="Delete driver"
             />
-            <span className={deleteStatus === "Driver deleted"? "success-message": "text-danger"}> {deleteStatus}</span>
           </div>
         </div>
       </form>
