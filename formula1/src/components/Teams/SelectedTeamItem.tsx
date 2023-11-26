0befb5db20752807e388158cfda333c27fe50395
Malin Akre <malin.akre01@gmail.com>
2023-11-26 20:17:42 +0100

    endring

--- formula1/src/components/Teams/SelectedTeamItem.tsx
+++ formula1/src/components/Teams/SelectedTeamItem.tsx
@@ -4,17 +4,12 @@ import { ITeam } from "../../interfaces/Teams/ITeam";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const SelectedTeamItem: FC<ITeam> = ({
-  id,
-  manufacturer,
   driver1,
-  driver2,
-  image,
+  driver2
 }) => {
   const modified_driver1 = driver1.replace(" ", "-");
   const modified_driver2 = driver2.replace(" ", "-");
 
-  const driver1FirstName = driver1.split(" ")[0];
-  const driver2FirstName = driver2.split(" ")[0];
   return (
     <div className="bg-image selected-team-bg">
       <div className="container">
