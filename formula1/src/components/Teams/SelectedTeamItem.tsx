944ab183117eb6d70825dc2138f4df64648ef0f6
eliasWolden <146863240+eliasWolden@users.noreply.github.com>
2023-11-26 19:08:49 +0100

    selected-tem

--- formula1/src/components/Teams/SelectedTeamItem.tsx
+++ formula1/src/components/Teams/SelectedTeamItem.tsx
@@ -12,11 +12,35 @@ const SelectedTeamItem: FC<ITeam> = ({ id, manufacturer, driver1, driver2, image
   const driver1FirstName = driver1.split(" ")[0];
   const driver2FirstName = driver2.split(" ")[0];
   return (
-    <div className="container">
-      <div className="bg-image">
+    <div className="bg-image selected-team-bg">
+      <div className="container">
+        <div className="row justify-content-end">
 
+          <div className="col-12 col-md-6">
+            <div className="card selected-team-card-size">
+            <img 
+              src={`http://localhost:5257/api/Image/driver/${modified_driver1}.png`}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{driver1}</h5>
+                <p className="card-text">Driver 1</p>
+              </div> 
+            </div>
+          </div>
+          <div className="col-12 col-md-6">
+            <div className="card selected-team-card-size">
+            <img 
+              src={`http://localhost:5257/api/Image/driver/${modified_driver2}.png`}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{driver2}</h5>
+                <p className="card-text">Driver 1</p>
+              </div> 
+            </div>
+          </div>
+
+        </div>
       </div>
-
     </div>
   );
 };
