4af5ab77bfe4af3dfb0554345a0b3b1caaef3530
eliasWolden <146863240+eliasWolden@users.noreply.github.com>
2023-11-26 19:30:49 +0100

    TEMP driver CRUD

---
+++ formula1/src/components/Drivers/ImageUpload.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+interface ImageUploadProps {
+  setHandler: (e: React.ChangeEvent<any>) => void;
+}
+
+const ImageUpload: React.FC<ImageUploadProps> = ({ setHandler }) => {
+  return (
+    <div className="row mb-3">
+      <div className="col-md-6">
+        <label htmlFor="image" className="form-label">
+          Upload image
+        </label>
+        <div className="custom-file">
+          <input
+            id="image"
+            name="image"
+            type="file"
+            className="custom-file-input"
+            onChange={setHandler}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ImageUpload;
