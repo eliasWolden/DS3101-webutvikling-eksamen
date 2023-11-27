be6e33463aa089f644e648645c1b910698baaa5f
bdybsjord <123090830+bdybsjord@users.noreply.github.com>
2023-11-27 03:14:10 +0100

    footer, crud

--- formula1/src/components/Home/Footer.tsx
+++ formula1/src/components/Home/Footer.tsx
@@ -1,13 +1,15 @@
 import { FC } from "react";
-import "../../css/HomePage.css";
 
 const Footer: FC = () => {
   return (
-    <footer className="bg-dark text-white py-5" role="contentinfo">
+    <footer
+      className="bg-dark text-white py-5 border-top border-black"
+      role="contentinfo"
+    >
       <div className="container">
         <div className="row">
           <div className="col-md-4 offset-md-4 text-center">
-            <section className="footer-section">
+            <section>
               <h3>About Our API</h3>
               <p>
                 Explore the capabilities of our API and integrate powerful
@@ -16,9 +18,9 @@ const Footer: FC = () => {
             </section>
           </div>
         </div>
-        <div className="text-center mt-4">
-          <p>&copy; 2023 Formula 1 Special Event. All rights reserved.</p>
-        </div>
+      </div>
+      <div className="text-center mt-4">
+        <p>&copy; 2023 Formula 1 Special Event. All rights reserved.</p>
       </div>
     </footer>
   );
